239c8b6db41b66e3e7b73ffe0ef6762b2875d4b0
jonayedahmad71-byte <jonayedahmad71@gmail.com>
2025-09-10 08:44:18 +0600

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname)));
 
 // SQLite Database
 const db = new sqlite3.Database('./chat-history.db');
